818519fc242cef9672b9b2cad3ad563444fc9d2e
Frank Liu <62361017+franciscoliu@users.noreply.github.com>
2025-03-12 11:11:34 -0400

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -110,7 +110,7 @@ def main(args):
 
     # LoRA configuration
     lora_config = LoraConfig(
-        r=8,
+        r=16,
         lora_alpha=16,
         lora_dropout=0.05,
         # target_modules=["q_proj", "v_proj"],
